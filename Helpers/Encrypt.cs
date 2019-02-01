b2951b395efcae6d39d9165ecd607685c08a6bec
Alejandro <alex>
2019-02-01 10:23:30 +0100

    Añadidos elementos helpers

---
+++ Helpers/Encrypt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace GttApiWeb.Helpers
+{
+    class Encrypt
+    {
+        public static string Hash(string value)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider mD5CryptoService = new MD5CryptoServiceProvider();
+            byte[] bytes = mD5CryptoService.ComputeHash(new UTF8Encoding().GetBytes(value));
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
+        }
+    }
+}
+
---
+++ Helpers/ResultError.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace GttApiWeb.Helpers
+{
+    public class ResultError
+    {
+        public string error { get; set; }
+        public int statusCode { get; set; }
+        public string mensaje { get; set; }
+        public string rol { get; set; }
+        public string jwt { get; set; }
+
+        public ResultError(String error,int statusCode,String mensaje)
+        {
+            this.error = error;
+            this.statusCode = statusCode;
+            this.mensaje = mensaje;
+        }
+
+        public ResultError(String error, int statusCode, String mensaje,String jwt,String rol)
+        {
+            this.error = error;
+            this.statusCode = statusCode;
+            this.mensaje = mensaje;
+            this.jwt = jwt;
+            this.rol = rol;
+        }
+    }
+}
+
