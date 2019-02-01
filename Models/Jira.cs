83f89ed76ec2c51bc735db98d02309b99d1578bf
Alejandro <alex>
2019-02-01 10:12:18 +0100

    Creados modelos

--- Models/Jira.cs
+++ Models/Jira.cs
@@ -7,6 +7,12 @@ namespace GttApiWeb.Models
 {
     public class Jira
     {
-  
+        public long id { get; set; }
+        public Users user_id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string url { get; set; }
+        public string proyect { get; set; }
+        public string componente { get; set; }
     }
 }
--- Models/Users.cs
+++ Models/Users.cs
@@ -9,6 +9,10 @@ namespace GttApiWeb.Models
 {
     public class Users
     {
-       
+        public long id { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public RolUser rolUser { get; set; }
     }
 }
