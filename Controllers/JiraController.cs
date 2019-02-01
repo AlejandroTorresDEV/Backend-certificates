8da47d8664065c9031b0b90da67de685658c2aa6
Alejandro <alex>
2019-02-01 10:17:47 +0100

    Creado Jira

--- Controllers/JiraController.cs
+++ Controllers/JiraController.cs
@@ -11,6 +11,51 @@ namespace GttApiWeb.Controllers
     [ApiController]
     public class JiraController : ControllerBase
     {
-    
+        private readonly AppDBContext _context;
+
+        public JiraController(AppDBContext context)
+        {
+            this._context = context;
+        }
+
+        // GET api/jira
+        [HttpGet]
+        public ActionResult<List<Jira>> Get()
+        {
+            return this._context.Jira.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public ActionResult<Jira> Post([FromBody] Jira value)
+        {
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine(value.ToString());
+            this._context.Jira.Add(value);
+            this._context.SaveChanges();
+            return value;
+            //Console.WriteLine("---------------"+value.Users.id);
+            //return null;
+        }
+
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }
+
